2dc96126ee39794b9637dfe117530e431edf99de
Szymon Paluch <12szymon.paluch34@gmail.com>
2020-12-27 17:57:39 +0100

    navbar

---
+++ src/components/nav.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react"
+import { Link } from "gatsby"
+
+const navClass = {
+    height: `6rem`, 
+    width: `100%`, 
+    padding: `0 4rem`,
+    display: `flex`, 
+    alignItems: `center`,
+    justifyContent: `flex-end`
+}
+const linkClass = {
+    fontSize: `1.3em`,
+    color: `black`, 
+    textDecoration: `none`,
+    margin: `0 1.2rem`
+}
+const underlineClass = {
+    height: `2px`,
+    transition: 'all .2s ease-in-out',
+    borderColor: `#000`,
+    marginBottom: `0`
+}
+
+//hover animation for links using hook
+const useUnderline = () => {
+    const [ underline, setUnderline ] = useState(0)
+  
+    const onMouseEnter = (e) => {
+        const num = parseInt(e.target.id.substring(4));
+        setUnderline(num);
+    }
+  
+    const onMouseLeave = () => {
+        setUnderline(0);
+    }
+  
+    const underlineAnimations = []
+    for(let i=1;i<4;i++) {
+        underlineAnimations.push(
+            underline === i ? {
+                width: `100%`
+            } : {
+                width: `0`
+            }
+        )
+    }
+  
+    return { underlineAnimations, onMouseEnter, onMouseLeave }
+  }
+
+const Nav = () => {
+    const { underlineAnimations, ...underlineProps} = useUnderline()
+
+    return (
+        <nav style={navClass}>
+            <Link id="nav-1" to="/thoughts" style={ linkClass } {...underlineProps}>
+                Thoughts 
+                <hr style={{ ...underlineClass, ...underlineAnimations[0] }}/> 
+            </Link>
+            <Link id="nav-2" to="/about" style={ linkClass } {...underlineProps}> 
+                About 
+                <hr style={{ ...underlineClass, ...underlineAnimations[1] }}/> 
+            </Link>
+            <Link id="nav-3" to="/contact" style={ linkClass } {...underlineProps}> 
+                Contact us 
+                <hr style={{ ...underlineClass, ...underlineAnimations[2] }}/> 
+            </Link>
+        </nav>
+    )
+}
+
+export default Nav
