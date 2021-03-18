9c0a52888419755fd4632c1ac5db4f9be01acfa9
Szymon Paluch <12szymon.paluch34@gmail.com>
2021-03-18 15:04:41 +0100

    apollo graphql - throws error 400, doesnt load images correctly

--- src/components/articleSection/article.js
+++ src/components/articleSection/article.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import styled from 'styled-components'
 import PropTypes from 'prop-types'
+import { gql, useQuery } from '@apollo/client'
+
+import styled from 'styled-components'
 import BackgroundImage from 'gatsby-background-image'
 
 import { SectionTitle } from '../../styles/styled'
@@ -15,35 +17,57 @@ const ArticleContentContainer = styled.div`
     padding: 2rem 0;
     width: 60%;
 `
+const DOWNLOAD_IMAGE = gql`
+query downloadImage($src: String!, $width: Int!){
+    img: file(relativePath: {eq: $src}) {
+        childImageSharp {
+            fluid(maxWidth: $width) {
+                ...GatsbyImageSharpFluid_noBase64
+            }
+        }
+    }
+}`
 
-const Article = (props, { className }) => (
-    <BackgroundImage
-        Tag="section"
-        className={ className }
-        fluid={props.img.childImageSharp.fluid} >
-        <ArticleElement>
-            <TextAnimation type="fade">
-                <SectionTitle> 
-                        Elit reprehenderit magna laborum esse culpa fugiat ipsum elit labore in. 
-                </SectionTitle>
-            </TextAnimation>
-            <ArticleContentContainer >
-                <TextAnimation type="fade" delay="600">
-                    <p> 
-                        Et ipsum pariatur reprehenderit est duis duis incididunt laborum consectetur fugiat.
-                    </p>
+const Article = (props, { className }) => {
+    const { loading, error, data } = useQuery(DOWNLOAD_IMAGE, {
+        variables: { src: "1.jpg", width: 1920 },
+    })
+
+    if (loading) return null;
+    if (error) {
+        console.log(error);
+        return null;
+    } 
+    
+    const imgData = data.img.childImageSharp.fluid;
+
+    return (
+        <BackgroundImage
+            Tag="section"
+            className={ className }
+            fluid={ imgData } >
+            <ArticleElement>
+                <TextAnimation type="fade">
+                    <SectionTitle> 
+                            Elit reprehenderit magna laborum esse culpa fugiat ipsum elit labore in. 
+                    </SectionTitle>
                 </TextAnimation>
-            </ArticleContentContainer>
-        </ArticleElement>
-    </BackgroundImage>
-)
+                <ArticleContentContainer>
+                    <TextAnimation type="fade" delay="600">
+                        Et ipsum pariatur reprehenderit est duis duis incididunt laborum consectetur fugiat.
+                    </TextAnimation>
+                </ArticleContentContainer>
+            </ArticleElement>
+        </BackgroundImage>
+    )
+}
 
 Article.propTypes = {
-    img: PropTypes.object
+    img: PropTypes.string
 }
 
 const StyledArticle = styled(Article)` 
-    height: 70vh;
+    height: 700px;
     background-size: cover;
 `
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,30 +8,39 @@ import SEO from "../components/seo"
 import { SectionTitle, SiteMotto } from '../styles/styled'
 import { TextAnimation } from '../components/animations'
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/__graphql',
+  cache: new InMemoryCache()
+})
+
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <SiteMotto>
-        <TextAnimation delay="400">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-        </TextAnimation>
-        <TextAnimation>
-            Nullam facilisis elit pretium, gravida ipsum at,
-        </TextAnimation>
-        <TextAnimation delay="600">
-            fermentum libero. 
-        </TextAnimation>
-    </SiteMotto>
+    <ApolloProvider client={client}>
+        <Layout>
+            <SEO title="Home" />
+            <SiteMotto>
+                <TextAnimation delay="400">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                </TextAnimation>
+                <TextAnimation>
+                    Nullam facilisis elit pretium, gravida ipsum at,
+                </TextAnimation>
+                <TextAnimation delay="600">
+                    fermentum libero. 
+                </TextAnimation>
+            </SiteMotto>
 
-    <ArticleSection />
+            <ArticleSection />
 
-    <SectionTitle centered id="thoughts">
-        <TextAnimation el="span"> Our </TextAnimation>
-        <TextAnimation el="span" type="slide-down"> Thoughts </TextAnimation>
-    </SectionTitle>
+            <SectionTitle centered id="thoughts">
+                <TextAnimation el="span"> Our </TextAnimation>
+                <TextAnimation el="span" type="slide-down"> Thoughts </TextAnimation>
+            </SectionTitle>
 
-    <MiniArticleSection />
-  </Layout>
+            <MiniArticleSection />
+        </Layout>
+    </ApolloProvider>
 )
 
 export default IndexPage
