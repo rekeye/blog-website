c2c3d91e4ec45dfba91fa8050b584bb4d2b3dfb2
Szymon Paluch <12szymon.paluch34@gmail.com>
2021-03-23 12:26:07 +0100

    change link in client

--- src/gatsby-theme-apollo/client.js
+++ src/gatsby-theme-apollo/client.js
@@ -4,7 +4,7 @@ import {ApolloClient, HttpLink, InMemoryCache} from '@apollo/client'
 const client = new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-        uri: "http://localhost:8000/__graphql",
+        uri: "/__graphql",
         fetch
     })
 });
