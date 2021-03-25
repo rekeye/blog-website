2b99ffdefdfe799585102a8f0bf8d085e3c717bd
Szymon Paluch <12szymon.paluch34@gmail.com>
2021-03-25 11:12:25 +0100

    adding plugin for svg to dependencies

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,9 +1,10 @@
 import React from "react"
-import { useQuery } from '@apollo/client'
-import { DOWNLOAD_FIXED_IMAGE } from '../graphql/downloadFixedImage'
 
-import Img from "gatsby-image"
 import styled from 'styled-components'
+
+import Hand from "../svg/logoHand.inline.svg"
+import Text from "../svg/logoText.inline.svg"
+
 
 const Container = styled.div`
   width: 225px;
@@ -13,31 +14,13 @@ const Container = styled.div`
   align-items: center;
 `
 
-const Logo = () => {
-  const { loading: textQueryLoading, error: textQueryError, data: text } = useQuery(DOWNLOAD_FIXED_IMAGE, {
-      variables: { src: "logo/text.png", width: 225 },
-  })
-  const { loading: handQueryLoading, error: handQueryError, data: hand } = useQuery(DOWNLOAD_FIXED_IMAGE, {
-    variables: { src: "logo/hand.png", width: 75 },
-  })
-
-  if (textQueryLoading || handQueryLoading) return null;
-  if (textQueryError) return console.log("error with logo query (text)");
-  if (handQueryError) return console.log("error with logo query (hand)");
-
-  console.log(text, hand);
-
-  const imgDataText = text.img.childImageSharp.fixed;
-  const imgDataHand = hand.img.childImageSharp.fixed;
-    
-  return (
-    <Container>
-      <div style={{ position: "absolute", }} >
-          <Img fixed={ imgDataText } />
-      </div>
-      <Img fixed={ imgDataHand } />
-    </Container>
-  );
-}
+const Logo = () => (
+  <Container>
+    <div style={{ position: "absolute", }} >
+        <Text />
+    </div>
+    <Hand />
+  </Container>
+)
 
 export default Logo
